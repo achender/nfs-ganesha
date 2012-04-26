f353d42d3c9c67fc4cf27119e6d265a84a14d641
Allison Henderson <achender@linux.vnet.ibm.com>
2012-04-26 15:12:22 -0700

    Add sockaddr_t param to FSAL_GetClientContext
    This patch adds a sockaddr_t* parameter to the FSAL_GetClientContext
    function.  This param will be later passed to the fsal specific
    funtions
    
    Signed-off-by: Allison Henderson <achender@linux.vnet.ibm.com>
    ------------------
    v1 -> v2
      Instead of passing the entire svc_req,
      we now only pass the sockaddr_t

--- src/support/nfs_client_id.c
+++ src/support/nfs_client_id.c
@@ -470,6 +470,7 @@ static void release_openstate(state_owner_t *popen_owner)
       /* Construct the fsal context based on the export and root credential */
       fsal_status = FSAL_GetClientContext(&fsal_context,
                                           &pstate_found->state_pexport->FS_export_context,
+                                          NULL,
                                           0,
                                           0,
                                           NULL,
@@ -558,6 +559,7 @@ void nfs_client_id_expire(nfs_client_id_t *client_record)
           /* construct the fsal context based on the export and root credential */
 	  fsal_status = FSAL_GetClientContext(&fsal_context,
                                       &plock_state->state_pexport->FS_export_context,
+                                      NULL,
                                       0,
                                       0,
                                       NULL,
